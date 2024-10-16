28d13399963add94d942675f372d55b51dbfb42a
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-16 11:44:40 +0800

    change routes

--- api/app.js
+++ api/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const products = require('./routes/product');
-const auth = require('./routes/auth');
-const order = require('./routes/order')
+const products = require('../routes/product');
+const auth = require('../routes/auth');
+const order = require('../routes/order')
 
 
 app.use(express.json({limit:'50mb'}));
